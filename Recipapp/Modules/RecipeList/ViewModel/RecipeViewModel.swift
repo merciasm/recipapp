3d18930b18f0c078463f3b4c77f5450cd872a00c
Mércia Maguerroski <mercia.maguerroski@gmail.com>
2024-04-30 17:25:38 -0300

    feat: Add viewModel

--- Recipapp/Modules/RecipeList/ViewModel/RecipeViewModel.swift
+++ Recipapp/Modules/RecipeList/ViewModel/RecipeViewModel.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+import RxSwift
+import RealmSwift
+
+class RecipeViewModel {
+    var items = PublishSubject<Results<RecipeDTO>>()
+    var onFetchEnd = PublishSubject<Bool>()
+
+    private var recipes: Results<RecipeDTO>?
+
+    func checkDataBase() {
+        recipes = DatabaseManager.shared.getAllItems()
+        if let recipes && recipes.isEmpty {
+            if  {
+                getAllRecipes()
+            } else {
+                items.onNext(recipes)
+            }
+
+
+    }
+
+    func getAllRecipes() {
+        guard let url = URL(string: Constants.URL.baseUrl) else { return }
+        APIManager.shared.fetchData(from: url, responseModel: RecipeList.self) { [weak self] result in
+            switch result {
+            case .success(let list):
+                if let recipeList = list as? RecipeList {
+                    let recipes = recipeList.recipes
+                    for recipe in recipes {
+                        let recipeDTO = RecipeDTO()
+                    }
+                    self?.items.onNext(recipes)
+                    self?.items.onCompleted()
+
+                    self?.onFetchEnd.onNext(true)
+                } else {
+                    self?.items.onNext([])
+                    self?.items.onCompleted()
+                    self?.onFetchEnd.onNext(true)
+                }
+            case .failure(let error):
+                self?.onFetchEnd.onNext(true)
+            }
+        }
+
+
+
+    }
+
+}
