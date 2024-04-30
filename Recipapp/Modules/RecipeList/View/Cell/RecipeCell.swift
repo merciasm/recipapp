7bbc69a031224da54a7f3c3bde2a0261eaba724a
Mércia Maguerroski <mercia.maguerroski@gmail.com>
2024-04-30 17:25:53 -0300

    feat: Add views

--- Recipapp/Modules/RecipeList/View/Cell/RecipeCell.swift
+++ Recipapp/Modules/RecipeList/View/Cell/RecipeCell.swift
@@ -5,4 +5,36 @@
 //  Created by Mércia Maguerroski on 4/29/24.
 //
 
-import Foundation
+import UIKit
+import AlamofireImage
+
+class RecipeCell: UITableViewCell {
+    // MARK: UI IBOUtlets
+    
+    @IBOutlet private var recipeRating: UILabel!
+    @IBOutlet private var recipeName: UILabel!
+    @IBOutlet private var recipeImage: UIImageView!
+    @IBOutlet private var recipeDifficulty: UILabel!
+    @IBOutlet private var recipeServingsNumber: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func setCellValues(recipe: Recipe) {
+        recipeName.text = recipe.name
+        recipeRating.text = "\(recipe.rating)"
+        recipeDifficulty.text = recipe.difficulty
+        recipeServingsNumber.text = "\(recipe.servings)"
+
+        setupImage(url: recipe.image)
+    }
+
+    private func setupImage(url: String) {
+        if let url = URL(string: url) {
+            recipeImage.af.setImage(withURL: url)
+        }
+    }
+
+}
--- Recipapp/Modules/RecipeList/View/RecipeListViewController.swift
+++ Recipapp/Modules/RecipeList/View/RecipeListViewController.swift
@@ -6,40 +6,63 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class RecipeListViewController: UIViewController {
 
     // MARK: Properties
     @IBOutlet private var tableView: UITableView!
+    private var viewModel = RecipeViewModel()
+    private var disposeBag = DisposeBag()
+    private let refreshControl = UIRefreshControl()
 
     // MARK: Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        setupPullToRefresh()
     }
-
 
     private func configureTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
+        tableView.register(UINib(nibName: "RecipeCell", bundle: nil), forCellReuseIdentifier: "RecipeCell")
+        bindTableData()
     }
 
-}
+    private func bindTableData() {
+        viewModel.items.bind(
+            to: tableView.rx.items(
+                cellIdentifier: "RecipeCell",
+                cellType: RecipeCell.self)
+        ) { row, model, cell in
+            cell.setCellValues(recipe: model.self)
+        }.disposed(by: disposeBag)
 
+        tableView.rx.modelSelected(Recipe.self).bind { recipe in
+            print(recipe)
+        }.disposed(by: disposeBag)
 
-extension RecipeListViewController: UITableViewDelegate {
+        viewModel.onFetchEnd.bind { onEnd in
+            if onEnd {
+                self.tableView.reloadData()
+                self.refreshControl.endRefreshing()
+            }
+        }.disposed(by: disposeBag)
 
-}
-
-extension RecipeListViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        viewModel.getAllRecipes()
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+
+    private func setupPullToRefresh() {
+        refreshControl.rx
+            .controlEvent(.valueChanged)
+            .subscribe(onNext: { [weak self] in
+                guard let self = self else { return }
+                self.refreshControl.beginRefreshing()
+                self.viewModel.getAllRecipes()
+            }).disposed(by: disposeBag)
+
+        tableView.refreshControl = refreshControl
     }
-    
 
 }
